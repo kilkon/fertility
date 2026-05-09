e3f477fd934d1c34af036986d050d88fd6d56c1e
Kilkon Ko <kilkon@snu.ac.kr>
2026-05-09 17:10:44 +0900

    Bust chart asset cache

--- scripts/build_book_pages.py
+++ scripts/build_book_pages.py
@@ -26,7 +26,7 @@ MANUSCRIPTS = ROOT / "manuscripts"
 CHAPTER_MANUSCRIPTS = MANUSCRIPTS / "chapters"
 SECTION_MANUSCRIPTS = MANUSCRIPTS / "sections"
 APPENDIX_FILE = "appendix-data-notes.html"
-ASSET_VERSION = "20260509-anonymous-feedback"
+ASSET_VERSION = "20260509-foreign-stat-gap"
 FEEDBACK_EMAIL = "kilkon@snu.ac.kr"
 PUBLIC_SITE_URL = "https://kilkon.github.io/fertility"
 
